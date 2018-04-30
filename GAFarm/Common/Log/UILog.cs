15f75e7fb52da22633038346343a7ba3f9156844
jeromepeng <snowxbear@163.com>
2018-04-30 23:58:18 +0800

    Add GA

---
+++ GAFarm/Common/Log/UILog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GAFarm.Common.Log
+{
+    public class UILog
+    {
+        private static TextBoxBase textBoxToLog = null;
+
+        private static UILog instance = null;
+
+        private UILog(TextBoxBase textBox)
+        {
+            textBoxToLog = textBox;
+        }
+
+        static public void CreateInstance(TextBoxBase textBox)
+        {
+            if (instance == null)
+            {
+                instance = new UILog(textBox);
+            }
+        }
+        static public void LogToTextBox(string logText)
+        {
+            if (instance != null)
+            {
+                textBoxToLog.Text += logText + "\n";
+            }
+        }
+    }
+}
